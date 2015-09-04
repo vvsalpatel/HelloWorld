def5dc6ffd53e2e1625fdc85975d5b893dbd013d
Uggla <uggla@free.fr>
2015-09-04 18:01:29 +0200

    :)

--- Code.py
+++ Code.py
@@ -1,8 +1,28 @@
+#!/usr/bin/python
+
 ###
 #
-#Header
-#Author Vincent Misson
-#Un peu plus de code !
-#Mais pas trop
+# Header
+# Author Vincent Misson
+# Un peu plus de code !
+# Mais pas trop
 #
 ###
+
+
+import sys
+import gzip
+import time
+
+print("\n")
+print("Uggla special code !")
+print("--------------------\n\n")
+
+with gzip.open('ascii.txt.gz', 'rb') as f:
+        file_content = f.read()
+
+print(file_content)
+time.sleep(3)
+
+while (True):
+    sys.stdout.write("'")
